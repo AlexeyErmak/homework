033c9dc7f3a5069879973f3f9f8a5d2d247348da
AlexeyErmak <leps.ermak10@gmail.com>
2026-01-15 20:56:41 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const burgerIcon = document.querySelector(".burger-icon");
-const menu = document.querySelector(".header__nav");
-
-burgerIcon.addEventListener("click", function () {
-  burgerIcon.classList.toggle("active");
-  menu.classList.toggle("active");
-});
